b97fe77062d3cfb2997a6e797b8b7d9dc4b6685b
Matthijs <matthijs_bierman@live.nl>
2018-02-28 16:05:01 +0100

    SmoothWalking WIP

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -20,20 +20,30 @@ public class Player : MonoBehaviour
 
     IEnumerator WalkWait()
     {
-        for (int i = 0; i < 10; i++)
+        for (int i = 0; i < 99; i++)
         {
 
             float input = Input.GetAxis("Vertical");
             yield return new WaitUntil(() => Input.GetAxis("Vertical") != 0);
 
-            WalkForward();
+            StartCoroutine(WalkForward());
 
             yield return new WaitForSeconds(0.5f);
         }
     }
 
-    void WalkForward()
+    IEnumerator WalkForward()
     {
-        transform.Translate(new Vector3(stepDistance, 0, 0));
+        Vector3 destination = transform.position + new Vector3(stepDistance, 0, 0);
+        Vector3 destinationFake = transform.position + new Vector3(stepDistance * 1.8f, 0, 0);
+        float offset = Vector3.Distance(transform.position, destinationFake);
+        while (offset > 0.8f)
+        {
+            offset = Vector3.Distance(transform.position, destinationFake);
+            transform.position = Vector3.Lerp(transform.position, destinationFake, stepDistance * Time.deltaTime);
+
+            yield return new WaitForEndOfFrame();
+        }
+        transform.position = destination;
     }
 }
