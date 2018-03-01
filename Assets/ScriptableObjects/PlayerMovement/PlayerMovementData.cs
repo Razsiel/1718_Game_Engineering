e390fdd9c8dbef77503fa8789819b13d7af3ee33
Matthijs <matthijs_bierman@live.nl>
2018-03-01 14:16:38 +0100

    ScriptableObject PlayerMovement and cleanup

---
+++ Assets/ScriptableObjects/PlayerMovement/PlayerMovementData.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Assets.ScriptableObjects.PlayerMovement
+{
+    [CreateAssetMenu]
+    public class PlayerMovementData : ScriptableObject
+    {
+        public float StepSize = 1f;
+        public float MovementSpeed = 1f;
+        public float OffsetTolerance = 0.001f;
+    }
+}
