e2c9f9f0aa26628dfedbad9c700b873b1bb81a99
geoffreyarkenbout@gmail.com <geoffreyarkenbout@gmail.com>
2018-03-01 18:52:02 +0100

    Switched project to .net 4.6 experimental. \nAdded Map data structure.

---
+++ Assets/Scripts/DataStructures/Map.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Assets.Scripts.DataStructures {
+    public class Map<T> : IReadOnlyCollection<MapEntry<T>> {
+        private readonly T[,] _map;
+
+        public int Width { get; }
+        public int Height { get; }
+
+        public int Count => Width * Height;
+
+        public Map(int width, int height) {
+            Width = width;
+            Height = height;
+            _map = new T[width, height];
+        }
+
+        public T this[int x, int y] {
+            get { return this._map[x, y]; }
+            set { this._map[x, y] = value; }
+        }
+
+        public IEnumerator<MapEntry<T>> GetEnumerator() {
+            for (int y = 0; y < this.Height; y++) {
+                for (int x = 0; x < this.Width; x++) {
+                    yield return new MapEntry<T>(this, x, y);
+                }
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+        
+    }
+}
---
+++ Assets/Scripts/DataStructures/MapEntry.cs
@@ -0,0 +1,17 @@
+﻿namespace Assets.Scripts.DataStructures {
+    public struct MapEntry<T> {
+
+        private readonly Map<T> _map;
+
+        public int X { get; }
+        public int Y { get; }
+
+        public MapEntry(Map<T> map, int y, int x) : this() {
+            this._map = map;
+            this.Y = y;
+            this.X = x;
+        }
+
+        public T Value => this._map[this.X, this.Y];
+    }
+}
