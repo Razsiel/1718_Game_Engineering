857b2bfd8002d9c8d3dd7058e6fd537346b16fb1
Razsiel <geoffreyarkenbout@gmail.com>
2018-03-19 15:12:57 +0100

    Finished the basic version of the grid editor. Added test tiles

--- Assets/ScriptableObjects/Tiles/TileData.cs
+++ Assets/ScriptableObjects/Tiles/TileData.cs
@@ -1,11 +1,16 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using UnityEngine;
 
 namespace Assets.ScriptableObjects.Tiles {
+    [Serializable]
     [CreateAssetMenu(menuName = "Data/Tiles/Tile")]
     public class TileData : ScriptableObject {
+        [SerializeField]
         public Mesh TileMesh;
+        [SerializeField]
         public TileDecorationData[] DecorationsData;
+        [SerializeField]
         public Material TileMaterial;
 		
 		//private event OnPlayerTileEnter;
