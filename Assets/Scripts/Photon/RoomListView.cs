51aacbdd59c45d0acdc12702994297692326e24d
vinnicool <lycazdrok@gmail.com>
2018-03-19 15:39:40 +0100

    no message

---
+++ Assets/Scripts/Photon/RoomListView.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class RoomListView : MonoBehaviour
+{
+    private List<GameObject> buttons = new List<GameObject>();
+    public GameObject buttonPrefab;
+    public GameObject panel;
+    public Vector3 margin;
+
+    public void UpdateListView(List<RoomInfo> rooms)
+    {
+        buttons.Clear();
+
+        for(int i = 0; i < rooms.Count; i++)
+        {
+            GameObject button;
+            if(i == 0)
+                button = GameObject.Instantiate(buttonPrefab, panel.transform.position + margin, Quaternion.identity);
+            else           
+                button = GameObject.Instantiate(buttonPrefab, buttons[i - 1].transform.position = margin, Quaternion.identity);
+        }
+    }
+
+    private void UpdateUI()
+    {
+        
+    }
+}
--- Assets/Scripts/Photon/RoomManager.cs
+++ Assets/Scripts/Photon/RoomManager.cs
@@ -4,23 +4,29 @@ using UnityEngine;
 using Photon;
 using UnityEngine.Events;
 using UnityEngine.Assertions;
+using System;
 
 public class RoomManager : TGEMonoBehaviour
 {
     public GameObject playerPrefab;
     private event UnityAction SpawnReplicated;
 
+    private List<RoomInfo> photonRooms;
+    private RoomListView roomView;
+
     //Lets connect two users to Photon and a lobby (+room)
     void Start()
     {
         PhotonNetwork.ConnectUsingSettings("1.0");
-
+        roomView = new RoomListView();
         Debug.Log("In Photon Start.");
         PhotonNetwork.autoJoinLobby = true;
         Debug.Log(PhotonNetwork.connectionState);
         PhotonManager.Instance.TGEOnJoinedLobby += () => 
         {
 
+            Array.ForEach(PhotonNetwork.GetRoomList(), x => photonRooms.Add(x));
+            roomView.UpdateListView(photonRooms);
             Debug.Log("We joined the lobby!");
             PhotonManager.Instance.TGEOnPhotonPlayerConnected += (PhotonPlayer player) =>
             {
