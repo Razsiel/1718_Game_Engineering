d71515c5c099d67d30d082a99fb30137b0f4b2eb
Vincent <lycazdrok@gmail.com>
2018-03-23 11:01:13 +0100

    no message

--- Assets/Scripts/Photon/PhotonManager.cs
+++ Assets/Scripts/Photon/PhotonManager.cs
@@ -4,6 +4,7 @@ using System;
 using UnityEngine.Events;
 using ExitGames.Client.Photon;
 using System.Collections.Generic;
+using System.Reflection;
 
 public class PhotonManager
 {
@@ -31,6 +32,25 @@ public class PhotonManager
     //private PhotonCallbacks callbacks = new PhotonCallbacks();
 
     private PhotonManager() { }
+ 
+    public void FireEvent(Guid instanceId, string handler)
+    {
+
+        // Note: this is being fired from a method with in the same class that defined the event (i.e. "this").
+        EventArgs e = new EventArgs();
+
+        MulticastDelegate eventDelagate =
+              (MulticastDelegate)this.GetType().GetField(handler,
+               System.Reflection.BindingFlags.Instance |
+               System.Reflection.BindingFlags.NonPublic).GetValue(this);
+
+        Delegate[] delegates = eventDelagate.GetInvocationList();
+
+        foreach(Delegate dlg in delegates)
+        {
+            dlg.Method.Invoke(dlg.Target, new object[] { this, e });
+        }
+    }
 
     #region PhotonCallbacks
     void OnJoinedLobby()
@@ -51,3 +71,28 @@ public class PhotonManager
     #endregion
 }
 
+public class PhotonCallbackReceiver : MonoBehaviour
+{
+
+    private void FireEvent(string eventName)
+    {
+        PhotonManager.Instance.FireEvent(new Guid(), eventName);
+    }
+
+    void OnJoinedLobby()
+    {
+        Debug.Log("InPUNCAll");
+        //TGEOnJoinedLobby.Invoke();
+        FireEvent(nameof(PhotonManager.Instance.TGEOnJoinedLobby));
+    }
+
+    void OnPhotonPlayerConnected(PhotonPlayer newPlayer)
+    {
+        //TGEOnPhotonPlayerConnected.Invoke(newPlayer);
+    }
+
+    void OnConnectedToPhoton()
+    {
+        Debug.Log("Connected");
+    }
+}
