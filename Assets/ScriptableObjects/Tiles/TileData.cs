173373a5a98deca5b5066f5deaa0ccefa691f975
Razsiel <geoffreyarkenbout@gmail.com>
2018-03-26 13:01:16 +0200

    Added a decorative tile which allows different sized tiles to be added to the level Also cleaned up gameobject creation for tiles. Duplicate code still exists and needs to be fixed later

--- Assets/ScriptableObjects/Tiles/TileData.cs
+++ Assets/ScriptableObjects/Tiles/TileData.cs
@@ -6,18 +6,39 @@ namespace Assets.ScriptableObjects.Tiles {
     [Serializable]
     [CreateAssetMenu(menuName = "Data/Tiles/Tile")]
     public class TileData : ScriptableObject {
-        [SerializeField]
-        public Mesh TileMesh;
-        [SerializeField]
-        public TileDecorationData[] DecorationsData;
-        [SerializeField]
-        public Material TileMaterial;
-		
-		//private event OnPlayerTileEnter;
-		//private event OnPlayerTileLeave;
+        [SerializeField] public Mesh TileMesh;
+        [SerializeField] public TileDecorationData[] DecorationsData;
+        [SerializeField] public Material TileMaterial;
 
-        public bool IsWalkable() {
+        //private event OnPlayerTileEnter;
+        //private event OnPlayerTileLeave;
+
+        public virtual bool IsWalkable() {
             return DecorationsData?.All(d => d.IsWalkable()) ?? false;
+        }
+
+        public GameObject GenerateGameObject(GameObject parent, bool hidden = false) {
+            return GenerateGameObject(parent.transform, hidden);
+        }
+
+        public GameObject GenerateGameObject(Transform parent, bool hidden = false) {
+            var tile = new GameObject("Tile",
+                                      typeof(MeshFilter),
+                                      typeof(MeshRenderer)) {
+                hideFlags = hidden ? HideFlags.HideAndDontSave : HideFlags.NotEditable
+            };
+
+            // Fill in components
+            var meshFilter = tile.GetComponent<MeshFilter>();
+            if (meshFilter != null) {
+                meshFilter.mesh = this.TileMesh;
+            }
+            var meshRenderer = tile.GetComponent<MeshRenderer>();
+            if (meshRenderer != null) {
+                meshRenderer.sharedMaterial = this.TileMaterial;
+            }
+
+            return tile;
         }
     }
 }
