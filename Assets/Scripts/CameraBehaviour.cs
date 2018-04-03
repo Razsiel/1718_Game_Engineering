d0dcf999477bf4678bf17074330a4a283ac0c00d
Matthijs <matthijs_bierman@live.nl>
2018-04-03 10:13:42 +0200

    Camera isometric setup

---
+++ Assets/Scripts/CameraBehaviour.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBehaviour : MonoBehaviour
+{
+    private Camera _camera;
+
+    // Use this for initialization
+    void Start ()
+    {
+        _camera = gameObject.GetComponent<Camera>();
+    }
+    
+    // Update is called once per frame
+    void Update () {
+        
+    }
+}
