3a1bfeff27234d2f6536c92613ae4cd6a7f3cd89
Razsiel <geoffreyarkenbout@gmail.com>
2018-04-06 16:38:44 +0200

    Added TileConfiguration for handling all possible permutations of TileData and DecorationData combinations.

---
+++ Assets/Scripts/DataStructures/TileConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using Assets.Data.Tiles;
+using UnityEngine;
+
+namespace Assets.Scripts.DataStructures {
+    [Serializable]
+    public class TileConfiguration {
+        [SerializeField] public TileData Tile;
+        [SerializeField] public TileDecorationData[] Decorations;
+        
+        public virtual bool IsWalkable(CardinalDirection direction)
+        {
+            return Decorations.Length == 0 || Decorations.All(d => d.IsWalkable(direction));
+        }
+
+        public virtual bool CanExit(CardinalDirection direction)
+        {
+            var oppositeDirection = direction.ToOppositeDirection();
+            return IsWalkable(oppositeDirection);
+        }
+
+    }
+}
