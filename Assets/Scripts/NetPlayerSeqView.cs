bed298ac1c57638b28303b19230d4230f909b168
Vincent <lycazdrok@gmail.com>
2018-04-13 16:55:09 +0200

    onnodige functie weggehaald

--- Assets/Scripts/NetPlayerSeqView.cs
+++ Assets/Scripts/NetPlayerSeqView.cs
@@ -33,10 +33,11 @@ namespace Assets.Scripts {
         }
 
         private void ClearCurrentCommands() {
-            foreach (var slot in SequenceBar.CommandSlots) {
-                var image = slot.transform.GetChild(0).GetComponent<Image>();
-                image.sprite = null;
-            }
+            //foreach (var slot in SequenceBar.CommandSlots) {
+            //    var image = slot.transform.GetChild(0).GetComponent<Image>();
+            //    image.sprite = null;
+            //}
+            SequenceBar.ClearImages();
         }
     }
 }
