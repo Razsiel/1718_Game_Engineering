0273a96753bcabc83aa723c3d92be785f623b202
yuop <youpkuiper@gmail.com>
2018-04-25 21:52:39 +0200

    Dynamic UI generation setup

--- Assets/Scripts/UI/DynamicUI.cs
+++ Assets/Scripts/UI/DynamicUI.cs
@@ -15,19 +15,68 @@ namespace Assets.Scripts.UI
 
         public GameObject CommandPanel;
         public GameObject SequenceBar;
-        public Player Player;
+        public GameObject MainSequenceBarPanel;
+        public GameObject Bottompanel;
         public CommandLibrary CommandLibrary;
 
         public void Start() {
             CreateCommands();
+            CreateSequenceBarSlots(13);
+        }
+
+        private void CreateSequenceBarSlots(int amountOfSlots)
+        {
+            List<Transform> sequenceBarPanels = new List<Transform>();
+
+            for (int i = 0; i < Bottompanel.transform.childCount; i++)
+            {
+                if (Bottompanel.transform.GetChild(i).transform.childCount > 0)
+                {
+                    sequenceBarPanels.Add(Bottompanel.transform.GetChild(i).gameObject.transform);
+                }
+            }
+
+            for (int i = 0; i < sequenceBarPanels.Count; i++)
+            {
+                var sequencebar = new GameObject();
+                sequencebar.name = "SequenceBar";
+                var sLayoutElement = sequencebar.AddComponent<LayoutElement>();
+                var sGridLayoutGroup = sequencebar.AddComponent<GridLayoutGroup>();
+
+                sLayoutElement.flexibleWidth = 7;
+                sGridLayoutGroup.padding.left = 20;
+                sGridLayoutGroup.childAlignment = TextAnchor.MiddleLeft;
+                sGridLayoutGroup.spacing = new Vector2(5f, 0f);
+
+                sequencebar.transform.SetParent(sequenceBarPanels[i], false);
+                sequencebar.transform.SetSiblingIndex(1);
+
+                if (sequencebar.transform.parent == sequenceBarPanels[0])
+                {
+                    sGridLayoutGroup.cellSize = new Vector2(50f, 50f);
+                }
+                else
+                {
+                    sGridLayoutGroup.cellSize = new Vector2(100f, 100f);
+                }
+
+
+                for (int j = 0; j < amountOfSlots; j++)
+                {
+                    var image = new GameObject(j.ToString());
+                    image.AddComponent<Image>();
+                    image.transform.SetParent(sequencebar.transform, false);
+                }
+            }
+
         }
 
         public void CreateCommands() {
             foreach (var command in CommandLibrary.Commands) { 
                 var uiCommand = CreateCommandButton(command.Value, CommandPanel, () => {
                     Debug.Log($"pressed a button");
-                    Player.SequenceChanged += OnSequenceChanged;
-                    Player.AddCommand(command.Value);
+                    //Player.SequenceChanged += OnSequenceChanged;
+                    //Player.AddCommand(command.Value);
                 });
             }
         }
@@ -50,19 +99,41 @@ namespace Assets.Scripts.UI
         }
 
         private GameObject CreateCommandButton(BaseCommand command, Transform parent, UnityAction onClick) {
-            var commandObject = new GameObject(command.ToString(), typeof(Image), typeof(Button));
-            commandObject.transform.parent = parent;
-            var rectTransform = commandObject.transform as RectTransform;
-            rectTransform.localScale = Vector3.one;
+            var commandObject = new GameObject(command.ToString());
+            commandObject.transform.SetParent(parent, false);
+            commandObject.AddComponent<LayoutElement>();
+            var commandHorizontalLayoutGroup = commandObject.AddComponent<HorizontalLayoutGroup>();
+            commandObject.AddComponent<CanvasRenderer>();
 
-            var image = commandObject.GetComponent<Image>();
-            Assert.IsNotNull(image);
-            image.sprite = command.Icon;
+            commandHorizontalLayoutGroup.padding.left = 10;
+            commandHorizontalLayoutGroup.childAlignment = TextAnchor.MiddleCenter;
+            commandHorizontalLayoutGroup.childControlHeight = true;
+            commandHorizontalLayoutGroup.childControlWidth = true;
+            commandHorizontalLayoutGroup.childForceExpandHeight = true;
+            commandHorizontalLayoutGroup.childForceExpandWidth = true;
 
-            var button = commandObject.GetComponent<Button>();
+            var text = new GameObject();
+            text.AddComponent<Text>();
+            text.GetComponent<Text>().font = Resources.GetBuiltinResource(typeof(Font), "Arial.ttf") as Font;
+            text.GetComponent<Text>().fontSize = 25;
+            text.GetComponent<Text>().alignment = TextAnchor.MiddleLeft;
+            text.GetComponent<Text>().text = command.Name;
+            text.AddComponent<LayoutElement>();
+            text.GetComponent<LayoutElement>().preferredWidth = 120;
+
+            var image = new GameObject();
+            image.AddComponent<Image>();
+            image.GetComponent<Image>().sprite = command.Icon;
+            image.GetComponent<Image>().preserveAspect = true;
+            image.AddComponent<LayoutElement>();
+            image.AddComponent<Button>();
+
+            var button = image.GetComponent<Button>();
             Assert.IsNotNull(button);
-            button.image = image;
             button.onClick.AddListener(onClick);
+
+            text.transform.SetParent(commandObject.transform, false);
+            image.transform.SetParent(commandObject.transform, false);
 
             return commandObject;
         }
