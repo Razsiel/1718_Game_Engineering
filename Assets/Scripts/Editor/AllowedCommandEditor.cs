9a4389a070a146cab14e3c895276d104dcb13b98
Razsiel <geoffreyarkenbout@gmail.com>
2018-04-30 12:55:28 +0200

    #GAME-111: - Added max commands allowed count for levels
    #GAME-110:
    - Added command whitelist in leveldata (gets automatically filled). List dize is still editible, but should NOT be touched!
    - If new command enums are added all allowed commands are reset (should be fixed, but low priority since commands are less likely to be added from now on)
    - Allowed commands can be gotten frm the GameInfo instance with the property AllowedCommands. This gets cached after first call since the method is costly due to crossreferencing enum values from two lists
    - Added a very basic custom editor that inheritently hides the ability to change the underlying enum value of the allowed command. Only shows a toggle box and a textfield
    
    OTHER:
    - Added the source file for the levelselect ui
    - Small update to UI.psd
    - Update to the level model (source file only since it's not finished

---
+++ Assets/Scripts/Editor/AllowedCommandEditor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Assets.Data.Levels;
+using UnityEditor;
+using UnityEngine;
+
+namespace Assets.Scripts.Editor
+{
+    [CustomPropertyDrawer(typeof(LevelData.AllowedCommand))]
+    public class AllowedCommandEditor : PropertyDrawer
+    {
+        private LevelData.AllowedCommand _target;
+
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label) {
+            SerializedProperty commandName =
+                property.FindPropertyRelative(nameof(LevelData.AllowedCommand.CommandType));
+            SerializedProperty isAllowedProp =
+                property.FindPropertyRelative(nameof(LevelData.AllowedCommand.IsAllowed));
+            int indent = EditorGUI.indentLevel;
+            isAllowedProp.boolValue =
+                EditorGUI.Toggle(position, ((CommandEnum)commandName.enumValueIndex).ToString(), isAllowedProp.boolValue);
+            EditorGUI.indentLevel = indent;
+        }
+    }
+}
