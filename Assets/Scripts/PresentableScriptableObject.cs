7688e8a1bade2b0ed394477bf1eee8f01f7eca2d
Razsiel <geoffreyarkenbout@gmail.com>
2018-05-01 10:18:15 +0200

    Started work on a new presentable implementation.

---
+++ Assets/Scripts/PresentableScriptableObject.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class PresentableScriptableObject : ScriptableObject, IPresentable {
+        [SerializeField] private List<PresentableConfiguration> _presentableConfigurations;
+        [HideInInspector] public List<PresentableConfiguration> PresentableConfigurations => _presentableConfigurations;
+
+        public virtual GameObject Present(Transform parent, bool hideInHierarchy = false, string name = "") {
+            return PresentablePresenter.Present(this, parent, hideInHierarchy, name);
+        }
+    }
+}
