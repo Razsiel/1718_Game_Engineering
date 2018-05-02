7db7a7e79a48727d4c5ab2f20ee110b4d5f9ca65
Razsiel <geoffreyarkenbout@gmail.com>
2018-05-02 10:03:54 +0200

    Added Main Menu with clicking behaviour

---
+++ Assets/Scripts/UI/MainMenuBehaviour.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using Assets.Scripts;
+using Assets.Scripts.DataStructures;
+using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.SceneManagement;
+using Utilities;
+
+namespace Assets {
+    public class MainMenuBehaviour : MonoBehaviour {
+        [SerializeField] private SceneField LevelSelect;
+        [SerializeField] private SceneField LobbyScene;
+
+        public void StartSingleplayer() {
+            Assert.IsNotNull(LevelSelect);
+            GlobalData.Instance.GameInfo.Players = new List<TGEPlayer> {
+                new TGEPlayer()
+            };
+            SceneManager.LoadScene(LevelSelect);
+        }
+
+        public void StartMultiplayer() {
+            Assert.IsNotNull(LobbyScene);
+            GlobalData.Instance.GameInfo.IsMultiplayer = true;
+            GlobalData.Instance.GameInfo.Players = new List<TGEPlayer> {
+                new TGEPlayer()
+            };
+            SceneManager.LoadScene(LobbyScene);
+        }
+    }
+}
