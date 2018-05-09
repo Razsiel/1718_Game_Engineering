578123047a3cdc720dd1a544dd1139ec5dda7aec
Matthijs <matthijs_bierman@live.nl>
2018-05-09 15:33:39 +0200

    IngameMenu concept added

--- Assets/Scripts/Behaviours/GameLoaderBehaviour.cs
+++ Assets/Scripts/Behaviours/GameLoaderBehaviour.cs
@@ -30,7 +30,7 @@ namespace Assets.Scripts.Behaviours {
                 manager.GameRoot = gameRoot;
                 manager.PlayerPrefab = _playerPrefab;
             });
-//            var audioManager = Instantiate(_audioPrefab, managersRoot.transform);
+            var audioManager = Instantiate(_audioPrefab, managersRoot.transform);
            
             var photonManager = Instantiate(_photonPrefab, managersRoot.transform);
             photonManager.GetComponent<PhotonManager>().CommandLib = _commandLibrary;
---
+++ Assets/Scripts/UI/IngameMenuBehaviour.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Assets.Scripts.DataStructures;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using Utilities;
+
+public class IngameMenuBehaviour : ModalPanel
+{
+    [SerializeField] private SceneField LevelSelect;
+    [SerializeField] private SceneField MainMenu;
+    [SerializeField] private Button BackToGame;
+    [SerializeField] private Button BackToMainMenu;
+    [SerializeField] private Button BackToLevelSelect;
+    [SerializeField] private Slider BgmVolumeSlider;
+    [SerializeField] private Slider SfxVolumeSlider;
+
+    void Awake()
+    {
+        this.Panel = this.gameObject;
+        Close();
+    }
+
+    public void OnClick_BackToGame()
+    {
+        Close();
+    }
+
+    public void OnClick_BackToMainMenu()
+    {
+        // Inform other player
+        // TableFlip
+        // Load scene Main.scene
+        SceneManager.LoadScene(MainMenu);
+    }
+
+    public void OnClick_BackToLevelSelect()
+    {
+        // Matthijs TODO: level/player lists?
+        // Inform other player
+        // TableFlip
+        // Load scene LevelSelect.scene
+        SceneManager.LoadScene(LevelSelect);
+    }
+
+    public void OnChanged_BgmVolumeSlider(Slider BgmSlider)
+    {
+        BgmBehaviour.Instance.SetVolume(BgmSlider.value);
+    }
+
+    public void OnChanged_SfxVolumeSlider(Slider SfxSlider)
+    {
+        SfxBehaviour.Instance.SetVolume(SfxSlider.value);
+    }
+
+    public override void Submit()
+    {
+        Close();
+    }
+
+    public override void Cancel()
+    {
+        Close();
+    }
+}
