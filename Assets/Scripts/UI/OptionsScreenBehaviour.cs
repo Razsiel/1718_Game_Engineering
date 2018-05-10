36dc5f65925a7cbd6142c173fb30dd6e3dc3db64
Matthijs <matthijs_bierman@live.nl>
2018-05-10 15:55:53 +0200

    Options Panel (WIP)

---
+++ Assets/Scripts/UI/OptionsScreenBehaviour.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using SmartLocalization;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using Utilities;
+
+public class OptionsScreenBehaviour : MonoBehaviour {
+    
+    [SerializeField] private Button BackToMainMenu;
+    [SerializeField] private Button LanguageFlagDutch;
+    [SerializeField] private Button LanguageFlagEnglish;
+    [SerializeField] private Slider BgmVolumeSlider;
+    [SerializeField] private Slider SfxVolumeSlider;
+
+    void Awake()
+    {
+        // Load settings from PlayerPrefs
+        HideOptionsPanel();
+    }
+
+    public void ShowOptionsPanel()
+    {
+        // Load settings from PlayerPrefs
+        gameObject.SetActive(true);
+    }
+
+    public void HideOptionsPanel()
+    {
+        gameObject.SetActive(false);
+    }
+
+    public void OnClick_BackToMainMenu()
+    {
+        // Hide this
+        HideOptionsPanel();
+
+        // show MainMenu
+
+    }
+
+    public void OnClick_LanguageFlagDutch()
+    {
+        // Set current language setting to Dutch
+        LanguageManager.Instance.ChangeLanguage("nl-NL");
+    }
+
+    public void OnClick_LanguageFlagEnglish()
+    {
+        // Set current language setting to English
+        LanguageManager.Instance.ChangeLanguage("en-GB");
+    }
+
+    public void OnChanged_BgmVolumeSlider(Slider BgmSlider)
+    {
+        BgmBehaviour.Instance.SetVolume(BgmSlider.value);
+    }
+
+    public void OnChanged_SfxVolumeSlider(Slider SfxSlider)
+    {
+        SfxBehaviour.Instance.SetVolume(SfxSlider.value);
+    }
+
+    private void SaveSettings()
+    {
+        // Save settings to PlayerPrefs
+    }
+}
