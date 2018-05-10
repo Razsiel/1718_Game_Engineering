bb490ace159c974e95b98d48399f5b5b638f08be
Vincent <lycazdrok@gmail.com>
2018-05-10 13:46:41 +0200

    Forgot to commit a few files.

---
+++ Assets/Scripts/Photon/LevelSelect/LevelSelectPhotonManager.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Assets.Data.Levels;
+using UnityEngine;
+using UnityEngine.UI;
+using Utilities;
+using MonoBehaviour = Photon.MonoBehaviour;
+
+namespace Assets.Scripts.Photon.LevelSelect
+{
+    public class LevelSelectPhotonManager : MonoBehaviour
+    {
+        public GameObject PlayButton;
+        public SceneField LevelScene;
+        private GameInfo _gameInfo;
+
+        public void Init(GameObject playButton, SceneField levelScene, GameInfo gameInfo)
+        {
+            this.PlayButton = playButton;
+            this.LevelScene = levelScene;
+            this._gameInfo = gameInfo;
+            this.photonView.viewID = (int)PhotonViewIndices.LevelSelect;
+            PlayButton.SetActive(PhotonNetwork.player.IsMasterClient);          
+        }
+
+        public void StartLevel(LevelData level)
+        {
+            this.photonView.RPC(nameof(SetLevel), PhotonTargets.Others, level);
+            PhotonNetwork.LoadLevel(LevelScene);
+        }
+
+        [PunRPC]
+        public void SetLevel(LevelData level, PhotonMessageInfo info)
+        {
+            _gameInfo.Level = level;
+        }
+    }
+}
---
+++ Assets/Scripts/Photon/PhotonViewIndices.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts.Photon
+{
+    public enum PhotonViewIndices
+    {
+        Main = 10,
+        RoomSelect,
+        LevelSelect,
+        InLevel
+    }
+}
