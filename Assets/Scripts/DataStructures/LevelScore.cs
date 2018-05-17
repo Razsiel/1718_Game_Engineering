0ce8938bda811169cee624957d22aae04caa95d4
vinnicool <lycazdrok@gmail.com>
2018-05-17 15:17:03 +0200

    Small code changes to calculating score

--- Assets/Scripts/DataStructures/LevelScore.cs
+++ Assets/Scripts/DataStructures/LevelScore.cs
@@ -12,19 +12,13 @@ namespace Assets.Scripts.DataStructures
     {
         public uint HighestScore;
         public uint DecentScore;
-        public uint BadScore;
+        //public uint BadScore;
 
-        public LevelScore(uint highestScore, uint decentScore, uint badScore)
+        public LevelScore(uint highestScore, uint decentScore/*, uint badScore*/)
         {
             this.HighestScore = highestScore;
             this.DecentScore = decentScore;
-            this.BadScore = badScore;
-            CheckValidValues();
-        }
-
-        private void CheckValidValues()
-        {            
-            Assert.IsTrue(DecentScore < BadScore && DecentScore > HighestScore);
-        }
+            //this.BadScore = badScore;            
+        }        
     }
 }
