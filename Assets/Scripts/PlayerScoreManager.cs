c132d24f2c6634c69da961162e4fb6a11e979833
yuop <youpkuiper@gmail.com>
2018-05-24 11:28:30 +0200

    Saving new score

--- Assets/Scripts/PlayerScoreManager.cs
+++ Assets/Scripts/PlayerScoreManager.cs
@@ -9,6 +9,7 @@ using Assets.Scripts.DataStructures;
 using System;
 using Assets.Scripts.Lib.Extensions;
 using System.Collections.Generic;
+using Assets.Data.Levels;
 
 public class PlayerScoreManager : MonoBehaviour
 {
@@ -73,8 +74,19 @@ public class PlayerScoreManager : MonoBehaviour
         print($"{nameof(PlayerScoreManager)}: Combined rounded equals {combinedStars}");
         
         Assert.IsTrue(combinedStars > 0 && combinedStars <= 3);
-      
+
+        IsNewHighScoreForLevel(_gameInfo.Level, combinedStars);
+        _gameInfo.Level.SaveScore(combinedStars);
         EventManager.PlayersScoreDetermined(playerScoreDic, combinedStars);
+    }
+
+    private void IsNewHighScoreForLevel(LevelData level, int newScore)
+    {
+        int oldScore = level.GetScore();
+        if (newScore > oldScore)
+        {
+            level.SaveScore(newScore);
+        } 
     }
 
     private int DetermineStars(int count, LevelScore levelScore)
