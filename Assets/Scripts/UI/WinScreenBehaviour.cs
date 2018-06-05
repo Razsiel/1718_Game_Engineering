6c99ddfb4a13a259826fa9c7bcb88a4bf10f1941
Razsiel <geoffreyarkenbout@gmail.com>
2018-06-05 12:35:06 +0200

    Fixed out of bounds exception in win screen score Client does not have buttons in winscreen anymore Command priority for movement and interact wasn't saved

--- Assets/Scripts/UI/WinScreenBehaviour.cs
+++ Assets/Scripts/UI/WinScreenBehaviour.cs
@@ -15,6 +15,7 @@ public class WinScreenBehaviour : MonoBehaviour {
     [SerializeField] private GameObject _playerScorePrefab;
     [SerializeField] private Image _scoreImage;
     [SerializeField] private Sprite[] _scoreImages;
+    [SerializeField] private GameObject[] _objectToDisableMP;
 
     private bool _isMultiplayer;
 
@@ -23,25 +24,23 @@ public class WinScreenBehaviour : MonoBehaviour {
         Assert.IsNotNull(_playerScorePrefab);
         Assert.IsNotNull(_scoreImage);
         Assert.IsNotNull(_scoreImages);
+        Assert.IsNotNull(_objectToDisableMP);
         GlobalData.SceneDataLoader.OnSceneLoaded += OnSceneLoaded;
     }
 
-    private void OnSceneLoaded(GameInfo gameInfo)
-    {
+    private void OnSceneLoaded(GameInfo gameInfo) {
         GlobalData.SceneDataLoader.OnSceneLoaded -= OnSceneLoaded;
         _isMultiplayer = gameInfo.IsMultiplayer;
         EventManager.OnPlayersScoreDetermined += ShowWinScreen;
         this.gameObject.SetActive(false);
     }
 
-    void OnDestroy()
-    {
+    void OnDestroy() {
         GlobalData.SceneDataLoader.OnSceneLoaded -= OnSceneLoaded;
         EventManager.OnPlayersScoreDetermined -= ShowWinScreen;
     }
 
-    private void ShowWinScreen(Dictionary<TGEPlayer, int> playerInfo, int totalScore)
-    {
+    private void ShowWinScreen(Dictionary<TGEPlayer, int> playerInfo, int totalScore) {
         EventManager.OnPlayersScoreDetermined -= ShowWinScreen;
         this.gameObject.SetActive(true);
 
@@ -52,32 +51,34 @@ public class WinScreenBehaviour : MonoBehaviour {
             playerScoreUiBehaviour.ShowPlayerScore(playerWithScore.Key, playerWithScore.Value, _isMultiplayer);
         }
 
+        if (_isMultiplayer) {
+            var isMaster = PhotonNetwork.player.IsMasterClient;
+            foreach (var go in _objectToDisableMP) {
+                go.SetActive(isMaster);
+            }
+        }
+
         //HOTFIX
         Canvas.ForceUpdateCanvases();
 
         List<string> compliments;
         switch (totalScore) {
             case 3:
-                compliments = new List<string>()
-                {
+                compliments = new List<string>() {
                     LanguageManager.Instance.GetTextValue("COMPLIMENT_THREE_STARS_1"),
                     LanguageManager.Instance.GetTextValue("COMPLIMENT_THREE_STARS_2"),
                     LanguageManager.Instance.GetTextValue("COMPLIMENT_THREE_STARS_3")
-
                 };
                 break;
             case 2:
-                compliments = new List<string>()
-                {
+                compliments = new List<string>() {
                     LanguageManager.Instance.GetTextValue("COMPLIMENT_TWO_STARS_1"),
                     LanguageManager.Instance.GetTextValue("COMPLIMENT_TWO_STARS_2"),
                     LanguageManager.Instance.GetTextValue("COMPLIMENT_TWO_STARS_3")
-
                 };
                 break;
             default:
-                compliments = new List<string>()
-                {
+                compliments = new List<string>() {
                     LanguageManager.Instance.GetTextValue("COMPLIMENT_ONE_STAR_1"),
                     LanguageManager.Instance.GetTextValue("COMPLIMENT_ONE_STAR_2")
                 };
@@ -85,11 +86,10 @@ public class WinScreenBehaviour : MonoBehaviour {
         }
 
         _complimentText.text = GetRandomfromList(compliments);
-        _scoreImage.sprite = _scoreImages[totalScore];
+        _scoreImage.sprite = _scoreImages[totalScore - 1];
     }
 
-    private string GetRandomfromList(List<string> listOfStrings)
-    {
+    private string GetRandomfromList(List<string> listOfStrings) {
         return listOfStrings[Random.Range(0, listOfStrings.Count)];
     }
 }
