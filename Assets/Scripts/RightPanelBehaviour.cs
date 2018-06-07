99ea7070e9c322d4496327b068f63fba9efd0378
yuop <youpkuiper@gmail.com>
2018-06-07 10:52:47 +0200

    input block fix

--- Assets/Scripts/RightPanelBehaviour.cs
+++ Assets/Scripts/RightPanelBehaviour.cs
@@ -1,35 +1,35 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using Assets.Scripts;
-using DG.Tweening;
-using UnityEngine;
-
-public class RightPanelBehaviour : MonoBehaviour {
-
-    private RectTransform _rectTransform;
-
-    Vector3 HidePosition = new Vector3(2304.0f, 324.1f, 0.0f);
-    Vector3 ShowPosition = new Vector3(1536.0f, 324.0f, 0.0f);
-
-    void Awake()
-    {
-        EventManager.OnInitializeUi += Initialize;
-        EventManager.OnUserInputEnable += ShowRightPanel;
-        EventManager.OnUserInputDisable += HideRightPanel;
-    }
-
-    void Initialize(GameInfo gameInfo)
-    {
-        _rectTransform = GetComponent<RectTransform>();
-    }
-
-    void HideRightPanel()
-    {
-        _rectTransform.DOLocalMove(HidePosition, 1f);
-    }
-
-    void ShowRightPanel()
-    {
-        _rectTransform.DOLocalMove(ShowPosition, 1f);
-    }
-}
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using Assets.Scripts;
+//using DG.Tweening;
+//using UnityEngine;
+//
+//public class RightPanelBehaviour : MonoBehaviour {
+//
+//    private RectTransform _rectTransform;
+//
+//    Vector3 HidePosition = new Vector3(2304.0f, 324.1f, 0.0f);
+//    Vector3 ShowPosition = new Vector3(1536.0f, 324.0f, 0.0f);
+//
+//    void Awake()
+//    {
+//        EventManager.OnInitializeUi += Initialize;
+//        EventManager.OnUserInputEnable += ShowRightPanel;
+//        EventManager.OnUserInputDisable += HideRightPanel;
+//    }
+//
+//    void Initialize(GameInfo gameInfo)
+//    {
+//        _rectTransform = GetComponent<RectTransform>();
+//    }
+//
+//    void HideRightPanel()
+//    {
+//        _rectTransform.DOLocalMove(HidePosition, 1f);
+//    }
+//
+//    void ShowRightPanel()
+//    {
+//        _rectTransform.DOLocalMove(ShowPosition, 1f);
+//    }
+//}
